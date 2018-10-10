b1026ed25707309618a3bea02a789392be32465a
ESC <youhavethewrong@gmail.com>
2018-10-09 22:14:44 -0500

    Add simple implementation of square root approximation function to be served up as a post endpoint.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,21 @@ use hyper::service::service_fn_ok;
 
 static TEXT: &str = "Hello, World!";
 
+fn square_root(square: i32) -> f32 {
+    let delta = 0.00001;
+    let mut last_guess = 0.0;
+    let mut current_guess = 1.0;
+    let mut difference = current_guess - last_guess as f32;
+
+    while difference.abs() > delta {
+        last_guess = current_guess;
+        current_guess = (last_guess + (square as f32 / last_guess) ) / 2.0;
+        difference = current_guess - last_guess;
+    }
+
+    return current_guess;
+}
+
 fn main() {
     let port = 3000;
     let addr = ([127, 0, 0, 1], port).into();
